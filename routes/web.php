56799d5ce890e1e003429a5f8e01d3c774d70c06
agsza <agszaasa88@gmail.com>
2025-11-02 22:34:12 +0700

    memperbaiki redirect

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BukuControllerr;
+use App\Http\Controllers\BukuController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,7 +16,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::resource('/buku', BukuController::class);
+    Route::resource('buku', BukuController::class);
 });
+
+
 
 require __DIR__.'/auth.php';
